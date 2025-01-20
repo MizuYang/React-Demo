fb3161859a5b046ece0527d9412edd06066fa2ce
Mizu <psdk55052004@gmail.com>
2025-01-20 15:06:05 +0800

    新增 練習: map 迴圈渲染元素

--- src/App.tsx
+++ src/App.tsx
@@ -10,17 +10,24 @@ import { Footer } from "@components/baseDemo/slots/footer.tsx";
 
 import { UseState } from "@components/baseDemo/UseState.tsx";
 
-import { Children } from '@components/baseDemo/Children.tsx';
+import { Children } from "@components/baseDemo/Children.tsx";
 
 function fatherFn() {
   console.log("父層的方法");
 }
 function ChildComponent() {
+  return <div>我是傳入的子元件~</div>;
+}
+
+function CardComponent({ product, idx }: { product: string; idx: number }) {
   return (
-    <div>
-      我是傳入的子元件~
+    <div className="ring ring-violet-400 p-3 my-4">
+      <h2>
+        {idx + 1}. {product}
+      </h2>
+      <p>卡片內容</p>
     </div>
-  )
+  );
 }
 function App() {
   return (
@@ -45,15 +52,18 @@ function App() {
           />
         </div>
 
+        {/* 屬性 props */}
         <div className="ring ring-blue-400 my-4">
           透過元件傳遞插槽:
           <Slots header={<Header />} main={<Main />} footer={<Footer />} />
         </div>
 
+        {/* useState */}
         <div className="ring ring-blue-400 my-4">
           <UseState />
         </div>
 
+        {/* children */}
         <div className="ring ring-blue-400 my-4">
           {/* 傳入元素 */}
           <Children>
@@ -64,6 +74,15 @@ function App() {
           <Children>
             <ChildComponent />
           </Children>
+        </div>
+
+        {/* map 迴圈 (v-for) */}
+        <div className="flex gap-x-3 ring ring-blue-400 my-4">
+          {["蛋糕", "布丁", "舒芙蕾", "泡芙"].map(
+            (product: string, idx: number) => {
+              return <CardComponent product={product} idx={idx} />;
+            }
+          )}
         </div>
       </div>
     </>
